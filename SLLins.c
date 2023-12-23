82729f08249087499613196226213e5ae5c9fc06
anurag-ms03 <anuragmsthefirst@gmail.com>
2023-12-23 19:34:16 +0530

    program for few operationa on singly linked list

--- SLLins.c
+++ SLLins.c
@@ -14,7 +14,7 @@ void printList(struct Node **head_ref){
         printf("%d->",temp->data);
         temp=temp->next;
       }
-      
+        printf("\n");
 
 }
 void push( struct Node **head_ref,int new_data){
@@ -29,8 +29,48 @@ void push( struct Node **head_ref,int new_data){
             (*head_ref)=new_node;
          }
 }
+void append(struct Node **head_ref,int new_data) {
+  struct Node *temp=(*head_ref);
+  struct Node *new_node =  (struct Node*)malloc(sizeof(struct Node));
+   new_node->data=new_data;
+   new_node->next=NULL;
+   if(temp==NULL){
+    (*head_ref)=new_node;
+    return;
+   }
+   while(temp->next!=NULL){
+        temp=temp->next;
+   }
+   temp->next=new_node;
+   
+}
+void insertafter(struct Node **head_ref,int prev_data,int new_data){
+        struct Node *prev = (*head_ref);
+        struct Node *new_node =  (struct Node*)malloc(sizeof(struct Node));
+        new_node->data=new_data;
+        if((*head_ref)==NULL){
+          (*head_ref)=new_node;
+          new_node->next=NULL;
+          return;
+        }
+        while(prev && prev->data != prev_data){
+               prev=prev->next;
+        }
+        if(prev==NULL)
+          printf("cannot insert\n");
+        else
+        {
+             new_node->next=prev->next;
+             prev->next=new_node;
+        }
+}
+
 int main(){
     struct Node *head = NULL;
-    push(&head,20);
+    append(&head,100);
     printList(&head);
+    insertafter(&head,100,60);
+    printList(&head);
+    
+    return 0;
 }
